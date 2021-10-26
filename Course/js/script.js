339a51bf3a4d837cbac084cc28a4ba132f950b81
Doctor-Hao <66946202+Doctor-Hao@users.noreply.github.com>
2021-10-26 09:34:36 +0300

    Add files via upload

---
+++ Course/js/script.js
@@ -0,0 +1,79 @@
+
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
+
+
+let burger = document.querySelector('.icon-menu');
+burger.addEventListener("click", function (e) {
+	let burger_icon = document.querySelector('.icon-menu');
+	let menu = document.querySelector('.header__menu');
+	let lock = document.querySelector('body');
+
+	burger_icon.classList.toggle('_active');
+	menu.classList.toggle('_active');
+	lock.classList.toggle('_lock');
+});
+
+
+//<Якоря>===========================================================================
+
+function forPeople(top)
+{
+	const about = document.querySelector(".want");
+	about.scrollIntoView({
+		block: "start",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+}
+function programmCourse(top)
+{
+	const about = document.querySelector(".education");
+	about.scrollIntoView({
+		block: "start",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+}
+function reviews(top)
+{
+	const about = document.querySelector(".reviews");
+	about.scrollIntoView({
+		block: "start",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+}
+function Cost(top)
+{
+	const about = document.querySelector(".price");
+	about.scrollIntoView({
+		block: "start",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+}
+function Form(top)
+{
+	const about = document.querySelector(".learn");
+	about.scrollIntoView({
+		block: "start",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+}
+//</Якоря>===========================================================================
